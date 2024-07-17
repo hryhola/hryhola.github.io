f61b5e8c233a4003bf2f562b4e6416b4bc69b140
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2024-07-17 16:25:11 +0300

    Contact item functionality. Added sass.

---
+++ src/lib/components/ContactItem.svelte
@@ -0,0 +1,180 @@
+<script lang="ts">
+    import { isContactsExpanded } from "$lib/scripts/context";
+
+    export let imgSrc: string
+    export let imgAlt: string
+    export let text: string
+    export let href: string
+    export let target: string | undefined = undefined
+    export let itemprop: string
+
+    let isLogoHover = false
+    let isTextHover = false
+    let isLinkFocus = false
+    let isButtonFocus = false
+
+    let tooltip: HTMLSpanElement;
+    let tooltipHideDebounce: number;
+
+    const setLogoHover = (value: boolean) => { isLogoHover = value }
+    const setTextHover = (value: boolean) => { isTextHover = value }
+    const setIsButtonFocus = (value: boolean) => { isButtonFocus = value }
+    const setIsLinkFocus = (value: boolean) => { 
+        if (value) {
+            isLinkFocus = true
+        } else {
+            setTimeout(() => {
+                isLinkFocus = false
+            }, 200);
+        }
+    }
+
+    const copyIntoClipboard = () => {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                if (tooltipHideDebounce) {
+                    clearTimeout(tooltipHideDebounce)
+                    setTimeout(() => {
+                        tooltip.dataset.active = 'false'
+                    }, 0)
+                }
+                setTimeout(() => {
+                    tooltip.dataset.active = 'true'
+                }, 100);
+
+                tooltipHideDebounce = setTimeout(() => {
+                    tooltip.dataset.active = 'false'
+                }, 1000)
+            })
+            .catch(e => console.error(e));
+    }
+
+    $: isActive = isLogoHover || isTextHover || isLinkFocus || isButtonFocus
+    $: copyButtonAttributes = isActive ? {} : { disabled: true }
+    $: tooltipAttributes = $isContactsExpanded ? {} : { hidden: true }
+</script>
+
+<dt
+    on:mouseenter={() => setLogoHover(true)}
+    on:mouseleave={() => setLogoHover(false)}
+>
+    <img height="16" src={imgSrc} alt={imgAlt} />
+</dt>
+<dd
+    on:mouseenter={() => setTextHover(true)}
+    on:mouseleave={() => setTextHover(false)}
+>
+    <a {href} {target} {itemprop} on:focus={() => setIsLinkFocus(true)} on:blur={() => setIsLinkFocus(false)} >{text}</a>
+    <div class="copy-wrapper">
+        <button
+            {...copyButtonAttributes}
+            on:focus={() => setIsButtonFocus(true)}
+            on:blur={() => setIsButtonFocus(false)}
+            on:click={copyIntoClipboard}
+        >
+            <img height="16" src="logos/copy.svg" alt="Copy" />
+        </button>
+        <span role="tooltip" {...tooltipAttributes} bind:this={tooltip} class="tooltip">Copied!</span>
+    </div>
+</dd>
+
+<style lang="scss">
+    @keyframes tooltip {
+        from {
+            opacity: 1;
+        }
+
+        to {
+            opacity: 0;
+        }
+    }
+
+    .tooltip {
+        position: absolute;
+        font-size: 12px;
+        margin-top: 9px;
+        margin-left: 9px;
+        opacity: 0;
+        pointer-events: none;
+        animation-play-state: paused;
+
+        &[data-active="true"] {
+            animation-name: tooltip;
+            animation-duration: 1s;
+            animation-play-state: running;
+        }
+    }
+
+    img {
+        filter: invert(1);
+    }
+
+    a {
+        margin-right: 6px;
+        text-decoration: none;
+        color: white;
+        transition: 0.2s;
+        outline-offset: 4px;
+
+        &[target='_blank']:hover {
+            text-decoration: underline;
+        }
+
+        &:focus {
+            outline: 1px solid white;
+            outline-offset: 0px;
+        }
+    }
+
+    dt,
+    dd {
+        height: 30px;
+        display: flex;
+        flex-flow: row;
+        flex-wrap: nowrap;
+        background: black;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    dt {
+        padding-left: 6px;
+        width: var(--marker-width);
+
+        &:hover + dd .copy-wrapper button {
+            width: 30px;
+        }
+    }
+
+    dd {
+        .copy-wrapper {
+            width: 30px;
+            background: white;
+        }
+
+        button {
+            width: 0px;
+            overflow: hidden;
+            height: 30px;
+            outline: none;
+            border: none;
+            cursor: pointer;
+            background: black;
+            transition: 0.2s;
+
+            &:focus {
+                width: 30px;
+                outline: 1px solid white;
+                outline-offset: -4px;
+            }
+        }
+
+        &:hover button {
+            width: 30px;
+        }
+
+        & a:focus ~ .copy-wrapper button {
+            width: 30px;
+        }
+    }
+</style>
--- src/lib/components/Contacts.svelte
+++ src/lib/components/Contacts.svelte
@@ -1,7 +1,41 @@
-<script>
+<script lang="ts">
     import { isContactsExpanded } from "$lib/scripts/context";
+    import ContactItem from "./ContactItem.svelte"
 </script>
-<style>
+<div id="contacts" data-is-open={$isContactsExpanded}>
+    <dl>
+        <ContactItem
+            href="https://t.me/VladHryhola"
+            target="_blank"
+            itemprop="sameAs"
+            text="@VladHryhola"
+            imgAlt="Telegram"
+            imgSrc="logos/telegram.svg"
+        />
+        <ContactItem
+            href="mailto:vladyslav.hryhola@gmail.com"
+            itemprop="email"
+            text="vladyslav.hryhola@gmail.com"
+            imgAlt="Email"
+            imgSrc="logos/email.svg"
+        />
+        <ContactItem
+            href="tel:+380669726801"
+            itemprop="telephone"
+            text="+38 066 972 6801"
+            imgAlt="Phone Number"
+            imgSrc="logos/phone.svg"
+        />
+        <ContactItem
+            href="https://linkedin.com/in/vlad-hryhola"
+            itemprop="url"
+            text="linkedin.com/in/vlad-hryhola"
+            imgAlt="Linked In"
+            imgSrc="logos/linkedin.svg"
+            target="_blank"
+        />
+</div>
+<style lang="scss">
     @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
     #contacts {
@@ -12,49 +46,18 @@
         font-family: "Fira Sans", sans-serif;
         font-size: 18px;
         font-style: normal;
-    }
 
-    #contacts a {
-        text-decoration: none;
-        color: black;
-    }
+        &[data-is-open="true"] {
+            grid-template-rows: 1fr;
+            padding-bottom: 6px;
+        }
 
-    #contacts dt, dd {
-        height: 30px;
-        display: grid;
-        align-items: center;
-    }
-
-    #contacts dt {
-        width: var(--marker-width);
-    }
-
-    #contacts dl {
-        overflow: hidden;
-        display: grid;
-        grid-template-columns: min-content 1fr;
-        align-items: start;
-        gap: 15px 0;
-
-    }
-
-    #contacts[data-is-open="true"] {
-        grid-template-rows: 1fr;
-        padding-bottom: 6px;
+        dl {
+            overflow: hidden;
+            display: grid;
+            grid-template-columns: min-content min-content;
+            align-items: start;
+            gap: 15px 0;    
+        }
     }
 </style>
-<div id="contacts" data-is-open={$isContactsExpanded}>
-    <dl>
-        <dt><img height="16" src="logos/telegram.svg" alt="Telegram"></dt>
-        <dd><a href="https://t.me/VladHryhola" target="_blank" itemprop="sameAs">@VladHryhola</a></dd>
-
-        <dt><img height="16" src="logos/email.svg" alt="Email"></dt>
-        <dd><a href="mailto:vladyslav.hryhola@gmail.com" itemprop="email">vladyslav.hryhola@gmail.com</a></dd>
-
-        <dt><img height="16" src="logos/phone.svg" alt="Phone Number"></dt>
-        <dd><a href="tel:+380669726801" itemprop="telephone">+38 066 972 6801</a></dd>
-
-        <dt><img height="16" src="logos/linkedin.svg" alt="Linked In"></dt>
-        <dd><a href="https://linkedin.com/in/vlad-hryhola" target="_blank" itemprop="url" rel="me">linkedin.com/in/vlad-hryhola</a></dd>
-    </dl>
-</div>
